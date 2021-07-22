c3cac32302335045d0eefd0039b95b35808f5b96
“ariellekatherine92” <ariellekatherine92@gmail.com>
2021-07-21 20:51:02 -0400

    added navigation links

--- react-refresher-cont/src/App.js
+++ react-refresher-cont/src/App.js
@@ -2,11 +2,13 @@ import { Route, Switch } from 'react-router-dom';
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupPage from './pages/NewMeetup';
 import FavoritesPage from './pages/Favorites';
+import MainNavigation from './components/layout/MainNavigation';
 
 function App() {
   return (
 <switch>
   <div>
+    <MainNavigation />
     <Route path='/' exact>
       <AllMeetupsPage />
     </Route>
