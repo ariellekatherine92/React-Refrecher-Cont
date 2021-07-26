5a97841511964ba77f560830da5b7ad5329968f2
“ariellekatherine92” <ariellekatherine92@gmail.com>
2021-07-26 15:01:41 -0400

    added more components and styling

--- react-refresher-cont/src/App.js
+++ react-refresher-cont/src/App.js
@@ -2,13 +2,13 @@ import { Route, Switch } from 'react-router-dom';
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupPage from './pages/NewMeetup';
 import FavoritesPage from './pages/Favorites';
-import MainNavigation from './components/layout/MainNavigation';
+import Layout from './components/layout/Layout';
+
 
 function App() {
   return (
+<Layout>
 <Switch>
-  <div>
-    <MainNavigation />
     <Route path='/' exact>
       <AllMeetupsPage />
     </Route>
@@ -18,8 +18,8 @@ function App() {
     <Route path='/favorites'>
       <FavoritesPage />
     </Route>
-  </div>
 </Switch>
+</Layout>
   );
 }
 
